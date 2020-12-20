d5705a8d61aebd3e5621fa285215a17a6ded5749
Brigitta Varga <v.brigitta.zs@gmail.com>
2020-12-20 22:04:39 +0100

    css

--- js/app.js
+++ js/app.js
@@ -1,20 +1,19 @@
 const emailToggle = document.querySelector('.email-toggle input');
 const profileToggle = document.querySelector(".profile-toggle input");
-const toggleOff = document.querySelector('.off');
+const toggleOff = document.querySelectorAll('.off');
 const toggleOn = document.querySelector(".on");
 
 emailToggle.addEventListener('click', () =>  {
   if (emailToggle.checked) {
-    toggleOff.style.display = 'none';
+    toggleOff[0].style.display = 'none';
   } else {
-    toggleOff.style.display = '';
+    toggleOff[0].style.display = '';
   }
 });
-
 profileToggle.addEventListener("click", () => {
     if (profileToggle.checked) {
-        toggleOff.style.display = "none";
+        toggleOff[1].style.display = "none";
     } else {
-        toggleOff.style.display = "";
+        toggleOff[1].style.display = "";
     }
 });
