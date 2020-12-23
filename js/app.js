9935721c199322470a9fb17daf2f83403e947c74
Brigitta Varga <v.brigitta.zs@gmail.com>
2020-12-23 20:50:11 +0100

    moved js to make charts appear

--- js/app.js
+++ js/app.js
@@ -40,12 +40,6 @@ let dailyChart = new Chart(dailyCanvas, {
   options: dailyOptions
   });
 
-let trafficChart = new Chart(trafficCanvas, {
-  type: "line",
-  data: trafficData,
-  option: trafficOptions
-});
-
 let trafficData = {
   labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
   "4-10", "11-17", "18-24", "25-31"],
@@ -74,6 +68,12 @@ let trafficData = {
     }
     };
 
+    let trafficChart = new Chart(trafficCanvas, {
+      type: "line",
+      data: trafficData,
+      option: trafficOptions
+    });
+    
     const mobileData = {
       labels: ["Desktop", "Tablet", "Phones"],
       datasets: [{
